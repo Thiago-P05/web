4c39799eb591a6f0daa56bc1da4ca6dd943b60e6
Thiago <thiagoperezng@gmail.com>
2025-08-19 21:04:15 -0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,14 @@
-// src/main.tsx
+// src/main.tsx  (o src/index.tsx según tu proyecto)
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import App from './App';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter basename="/web">
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
